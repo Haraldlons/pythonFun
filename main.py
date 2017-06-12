4073e115632ec731549dd7f91d5278d97752f90c
Haraldlons <haraldlons@gmail.com>
2017-06-12 11:31:41 +0200

    First commit. Added all current files. Both files are for testing purposes

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello World")
+
+for i in range(19):
+	print(str(i) + " Hello")
+
+x = input("Type your input: ")
+
+print("You typed: " + str(x))
---
+++ plotting.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,3,6,9])
+plt.ylabel('some numbers')
+plt.xlabel('some more numbers')
+plt.show()
